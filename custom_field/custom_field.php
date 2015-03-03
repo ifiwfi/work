2604173d3a078abc8e7a9067ea72b92ac76a5006
Irfan Ahmed <irfan.ahmed@timegroup.ae>
2015-03-03 02:20:34 -0800

    Custom field plugin

---
+++ custom_field/custom_field.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+	Plugin Name: Custom field in post page
+	Description: With this plugin, you can add a custom field in your post page
+	Author: Irfan Ahmed
+	Email: irfan.ahmed5123@yahoo.com
+	Version: 1.0
+	*/	
+	include('class_custom_field.php');
+?>
